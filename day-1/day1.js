ce687cb89306b55c8dcf940db5cd098737624dd0
milon27 <mdjahidulislammilon@gmail.com>
2021-12-07 19:10:02 +0600

    solved a problem with filter map arr

---
+++ day-1/day1.js
@@ -0,0 +1,51 @@
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+];
+
+fdfkdk
+// git add .
+//git commit -m 'any msge'
+//git push
+
+// let's say you have this student array.
+
+// q1.print all the student name with grade.
+//     q2.update Jason grade to 99.
+// q3.remove Sam from the list.
+
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+//forEach, map(update), filter(delete)
+
+// nums.forEach(function (item) { //loop though the items.
+//     console.log("item: ", item);
+// })
+
+//map
+
+// const num2 = nums.map(function (item) {
+//     if (item === 3) {
+//         return item * 2;
+//     } else {
+//         return item
+//     }
+// })
+
+// console.log(num2);
+
+// //filter
+
+// const arr = nums.filter(function (item) {
+//     if (item === 3) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// })
+
+// console.log(arr);
